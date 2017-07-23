52d9e895e0a7edd933296f71c78e62966d54f9c6
chuong2v <chuong.2v@gmail.com>
2017-07-23 18:54:04 +0700

    [set up] - refactor api router

--- core/routes.js
+++ core/routes.js
@@ -1,6 +1,9 @@
-import routes from './../api/configs/routes'
+import _ from 'lodash'
+import Controllers from './controllers'
+import Policies from './policies'
+import routes from './../api/routes'
 
-export default routes.map(route => {
+let transformedRoutes = routes.map(route => {
   let list = route.trim().split(' ').filter(item => item);
   return {
     method: list[0].trim(),
@@ -10,3 +13,28 @@ export default routes.map(route => {
     policies: (list[4] || "").split(',').map(item => item.trim())
   }
 })
+
+export default (server) => {
+  transformedRoutes.forEach(route => {
+    if (Array.isArray(route.policies) && route.policies.length > 0) {
+      route.policies.forEach(policy => server.use(Policies[policy]))
+    } else if (Policies.default) {
+      server.use(Policies[Policies.default])
+    }
+
+    server[route.method](route.url, (req, res) => {
+      let ControllerClass = Controllers[route.controller];
+      if (ControllerClass) {
+        let controllerObject = new ControllerClass(req, res);
+        let action = controllerObject[route.action];
+        if (_.isFunction(action)) {
+          action.call(controllerObject);
+        } else {
+          res.send({ error: "Action not found." })
+        }
+      } else {
+        res.send({ error: "Action not found." })
+      }
+    })
+  })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,6 @@
 import _ from 'lodash'
-import { Routes } from './../core'
-import { Controllers, Policies } from './../core'
+import { ApiRouter } from './../core'
 
 export default (server) => {
-  Routes.forEach(route => {
-    if (Array.isArray(route.policies) && route.policies.length > 0) {
-      route.policies.forEach(policy => server.use(Policies[policy]))
-    } else if (Policies.default) {
-      server.use(Policies[Policies.default])
-    }
-
-    server[route.method](route.url, (req, res) => {
-      let ControllerClass = Controllers[route.controller];
-      if (ControllerClass) {
-        let controllerObject = new ControllerClass(req, res);
-        let action = controllerObject[route.action];
-        if (_.isFunction(action)) {
-          action.call(controllerObject);
-        } else {
-          res.send({ error: "Action not found." })
-        }
-      } else {
-        res.send({ error: "Action not found." })
-      }
-    })
-  })
+  ApiRouter(server)
 }
